be0c9112cee581c402b38be8d1872d0c5d2eec18
Brendon Murphy <xternal1+github@gmail.com>
2011-03-29 21:41:36 -0700

    pluralize drink resource in test.

--- test/as_json.rb
+++ test/as_json.rb
@@ -4,19 +4,19 @@ require 'json'
 test "set a block to return json" do
   rum_and_coke = { "rum" => "hot", "coke" => "sweet" }
   Cuba.define do
-    on "drink" do
-      as_json 201, {"Content-Location" => "http://somewhere.com/drink/42"} do
+    on "drinks" do
+      as_json 201, {"Content-Location" => "http://somewhere.com/drinks/42"} do
         rum_and_coke
       end
     end
   end
 
-  env = { "SCRIPT_NAME" => "/", "PATH_INFO" => "/drink" }
+  env = { "SCRIPT_NAME" => "/", "PATH_INFO" => "/drinks" }
 
   _, _, resp = Cuba.call(env)
 
   assert_equal 201, resp.status
   assert_equal "application/json", resp.headers["Content-Type"]
   assert_equal [rum_and_coke.to_json], resp.body
-  assert_equal "http://somewhere.com/drink/42", resp.headers["Content-Location"]
+  assert_equal "http://somewhere.com/drinks/42", resp.headers["Content-Location"]
 end
