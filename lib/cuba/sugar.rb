46d4434f843a1ac502361814bd01806a509f4ddb
Bruno Aguirre <yo@brunoaguirre.com>
2011-09-13 19:35:23 -0300

    Fix compatibility

--- lib/cuba/sugar.rb
+++ lib/cuba/sugar.rb
@@ -1,26 +1,24 @@
 require 'json'
 
-module Cuba
-  class Ron
-    # Sugar to do some common response tasks
-    #
-    # @example
-    # on post, "users" do
-    #   as 201 do
-    #     "User successfully created!"
-    #   end
-    # end
-    def as(http_code = 200, extra_headers = {})
-      res.status = http_code
-      res.headers.merge! extra_headers
-      res.write yield if block_given?
-    end
+class Cuba
+  # Sugar to do some common response tasks
+  #
+  # @example
+  # on post, "users" do
+  #   as 201 do
+  #     "User successfully created!"
+  #   end
+  # end
+  def as(http_code = 200, extra_headers = {})
+    res.status = http_code
+    res.headers.merge! extra_headers
+    res.write yield if block_given?
+  end
 
-    def as_json(http_code = 200, extra_headers = {})
-      extra_headers["Content-Type"] ||= "application/json"
-      as(http_code, extra_headers) do
-        (yield).to_json if block_given?
-      end
+  def as_json(http_code = 200, extra_headers = {})
+    extra_headers["Content-Type"] ||= "application/json"
+    as(http_code, extra_headers) do
+      (yield).to_json if block_given?
     end
   end
 end
